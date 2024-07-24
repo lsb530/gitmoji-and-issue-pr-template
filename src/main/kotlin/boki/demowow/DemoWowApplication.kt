7d53e6296657bfa55e05c5c61382eef18b7d37a5
boki <lsb530@naver.com>
2024-07-25 03:51:50 +0900

    chore: pr test

--- src/main/kotlin/boki/demowow/DemoWowApplication.kt
+++ src/main/kotlin/boki/demowow/DemoWowApplication.kt
@@ -7,6 +7,6 @@ import org.springframework.boot.runApplication
 class DemoWowApplication
 
 fun main(args: Array<String>) {
-    println("PR")
+    println("PR2")
     runApplication<DemoWowApplication>(*args)
 }
